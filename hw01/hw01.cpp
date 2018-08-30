19f9eb318b68ad9caa48a54f77fe1c51e99fa000
Christopher Sparks <csparks1@my.hpu.edu>
2018-08-29 16:15:51 -1000

    hw01 commited

--- hw01/hw01.cpp
+++ hw01/hw01.cpp
@@ -1,1 +1,11 @@
 #include <cstdio>
+
+double celsius2fahrenheit(double celsius);
+
+int main (int argc, char** argv){
+    printf("%lf\n",celsius2fahrenheit(100));
+}
+
+double celsius2fahrenheit(double celsius){
+    return (celsius*(9.0/5)+32);
+}
